5a24058fed7ba6d04dc395def88a57b71e161054
ghanbari <ghanbari@ualberta.ca>
2020-10-03 12:01:21 -0600

    refractored

--- react-front-end/src/components/Main/Main.jsx
+++ react-front-end/src/components/Main/Main.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import './Main.scss';
 
 import SectionOne from './SectionOne';
-
+import SectionGrid from './SectionGrid';
 
 
 function Main (props) {
 
+  const gridInput = {
+    e1: ["Enhance Oil Recovery"],
+    e2: ["Optimize Production"],
+    e3: ["Request Demo"]
+  }
+
+
   return (
     <div>
       <SectionOne />
+      <SectionGrid 
+      gridInput = {gridInput} />
     </div>
   );
 
