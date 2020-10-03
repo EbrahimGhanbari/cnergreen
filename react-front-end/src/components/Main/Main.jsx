5fd9c6f552c157360d9b3fd5ac5d8b7640d38034
ghanbari <ghanbari@ualberta.ca>
2020-10-02 19:14:37 -0600

    added elements

--- react-front-end/src/components/Main/Main.jsx
+++ react-front-end/src/components/Main/Main.jsx
@@ -7,13 +7,29 @@ import { Link } from "react-router-dom";
 function Main (props) {
 
   return (
-    <div>
-      <div>
-        <Link className="topButton" to="/home">Home</Link>
-        <Link className="topButton" to="/solution">Solution</Link>
-        <Link className="topButton" to="/blog">Blog</Link>
-        <Link className="topButton" to="/team">Team</Link>
-        <Link className="topButton" to="/contact">Contact</Link>
+    <div className="topSection">
+      <div className="topNav">
+        <div>
+          <img id="logo" src="cat.jpg" alt="Italian Trulli"/>
+        </div>
+        <div className="topButtonGroup">
+          <Link className="topButton" to="/home">Home</Link>
+          <Link className="topButton" to="/solution">Solution</Link>
+          <Link className="topButton" to="/blog">Blog</Link>
+          <Link className="topButton" to="/team">Team</Link>
+          <Link className="topButton" to="/contact">Contact</Link>
+        </div>
+      </div>
+      <div className="topText">
+        <div className="title">
+          Clean and Efficient Hydrocarbon Production
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        </p>
+        <button>
+          Learn more
+        </button>
       </div>
     </div>
   );
