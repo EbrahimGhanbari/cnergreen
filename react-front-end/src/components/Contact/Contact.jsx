f7f750fc4f3cbc6a3e93e2681b21ec1fc2653417
ghanbari <ghanbari@ualberta.ca>
2020-10-15 15:24:36 -0600

    added styling

--- react-front-end/src/components/Contact/Contact.jsx
+++ react-front-end/src/components/Contact/Contact.jsx
@@ -3,47 +3,11 @@ import React from 'react';
 
 // import './SectionOne.scss';
 import { Link } from "react-router-dom";
-import { FaRegHandPointRight } from 'react-icons/fa';
 
 import './Contact.scss'
 
 
 function Contact (props) {
-
-
-//   return (
-    // <div className="topSection">
-    //   <div className="topNav">
-    //     <div>
-    //       <img id="logo" src="cnergreenlogo.png" alt="Italian Trulli"/>
-    //     </div>
-    //     <div className="topButtonGroup">
-    //       <Link className="topButton" to="/home">Home</Link>
-    //       <Link className="topButton" to="/solution">Solution</Link>
-    //       <Link className="topButton" to="/blog">Blog</Link>
-    //       <Link className="topButton" to="/team">Team</Link>
-    //       <Link className="topButton" to="/contact">Contact</Link>
-    //     </div>
-    //   </div>
-    //   <div className="topText">
-    //     <div className="titleMain">
-    //       Clean and Efficient Hydrocarbon Production
-    //     </div>
-    //     <div className="txtAndButton">
-    //       <div className="textAfterTitle">
-    //         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-    //       </div>
-    //       <div>
-    //       <button className="learnMoreButton">Learn More <FaRegHandPointRight id="learnMoreIcon" /></button>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-//   );
-
-
-
-
 
 
   const [message, setMessage] = useState({})
@@ -61,8 +25,8 @@ function Contact (props) {
       case "email":
         setMessage({...message, email: event.target.value});
         break;
-      case "title":
-        setMessage({...message, title: event.target.value});
+      case "name":
+        setMessage({...message, name: event.target.value});
         break;
       case "content":
         setMessage({...message, content: event.target.value});
@@ -87,22 +51,26 @@ function Contact (props) {
           <Link className="topButton" to="/contact">Contact</Link>
         </div>
       </div>
-        <form >
-          <p>What Can We Help You With?</p>
-          <label> Email
-            <input type="text" id="email" onChange= { handleChange }></input>
-          </label>
-          <label> Title
-            <input type="text" id="title"  onChange= { handleChange }></input>
-          </label>
-          <label> Content
-            <input type="text" id="content"  onChange= { handleChange }></input>
-          </label>
+      <div className="contactUsParent">
 
-        <input type="submit" value="Submit" onSubmit={handleSubmit} />
+        <form className="contactUsForm">
+          <p className="contactUsTitle">What Can We Help You With?</p>
+          <label>
+            <input className="contactUs" placeholder="Name" type="text" id="name"  onChange= { handleChange }/>
+          </label>
+          <label> 
+            <input className="contactUs" placeholder="Email" type="text" id="email" onChange= { handleChange }/>
+          </label>
+          <label>
+            <textarea className="contactUs" placeholder="Your Inquery" type="text" id="content"  onChange= { handleChange }/>
+          </label>
+          <label>
+            <input className="contactUs" type="submit" value="Submit" id="contactUsSubmit" onSubmit={handleSubmit} />            
+          </label>
       </form>
+      </div>
       {/* <div className="topText">
-        <div className="titleMain">
+        <div className="tit leMain">
           Clean and Efficient Hydrocarbon Production
         </div>
         <div className="txtAndButton">
