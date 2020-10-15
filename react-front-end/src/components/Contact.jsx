246ee55fa54abee67cf8084da5ac9f47b0b7c7f0
ghanbari <ghanbari@ualberta.ca>
2020-10-15 11:34:12 -0600

    updated

--- react-front-end/src/components/Contact.jsx
+++ react-front-end/src/components/Contact.jsx
@@ -1,12 +1,55 @@
+import { useState } from "react";
 import React from 'react';
 
 
 function Contact (props) {
 
+
+
+  const [message, setMessage] = useState({})
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+
+  };
+
+  const handleChange = (event) => {
+    event.preventDefault();
+
+    switch (event.target.id) {
+      case "email":
+        setMessage({...message, email: event.target.value});
+        break;
+      case "title":
+        setMessage({...message, title: event.target.value});
+        break;
+      case "content":
+        setMessage({...message, content: event.target.value});
+        break;
+    }
+
+  };
+
+
+
   return (
-    <div>
-      <h1>THIS IS Contact</h1>
-    </div>
+    <form >
+      <label>
+      Email
+        <input type="text" id="email" onChange= { handleChange }></input>
+      </label>
+      <label>
+        Title
+        <input type="text" id="title"  onChange= { handleChange }></input>
+      </label>
+      <label>
+      Content
+        <input type="text" id="content"  onChange= { handleChange }></input>
+      </label>
+
+      <input type="submit" value="Submit" onSubmit={handleSubmit} />
+    </form>
   );
     
 }
