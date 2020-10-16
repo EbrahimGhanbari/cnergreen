44aa9c1feafda5e9cb677d2de307bc57046bbeaf
ghanbari <ghanbari@ualberta.ca>
2020-10-16 11:29:23 -0600

    data communication btw backend and fronend working

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,19 +1,39 @@
-const Express = require('express');
+const Express = require("express");
 const App = Express();
-const BodyParser = require('body-parser');
-const PORT = 8080;
+const Logger = require("morgan");
+const BodyParser = require("body-parser");
+const cors = require("cors");
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
-App.use(Express.static('public'));
+App.use(Express.static("public"));
+App.use(Logger("dev"));
+
+App.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 // Sample GET route
-App.get('/api/data', (req, res) => res.json({
-  message: "Seems to work yay!",
-}));
+App.get("/api/data", (req, res) => {
+  res.json({
+    message: "Seems to work yay!",
+  });
+});
 
+App.post("/message", (req, res) => {
+  console.log(req.body);
+});
+
+const PORT = 8080;
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+  console.log(
+    `Express seems to be listening on port ${PORT} so that's pretty good 👍`
+  );
 });
+
+const nodemailer = require("nodemailer");
--- react-front-end/src/components/Contact/RequestInfo.jsx
+++ react-front-end/src/components/Contact/RequestInfo.jsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
 import TopNavBar from "../TopNavBar";
 
-import './RequestInfo.scss'
+import "./RequestInfo.scss";
 
-function Contact (props) {
-
-
-  const [message, setMessage] = useState({})
+function Contact(props) {
+  const [message, setMessage] = useState({});
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    axios
+      .post("http://localhost:8080/a", message)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   const handleChange = (event) => {
@@ -19,16 +26,15 @@ function Contact (props) {
 
     switch (event.target.id) {
       case "email":
-        setMessage({...message, email: event.target.value});
+        setMessage({ ...message, email: event.target.value });
         break;
       case "name":
-        setMessage({...message, name: event.target.value});
+        setMessage({ ...message, name: event.target.value });
         break;
       case "content":
-        setMessage({...message, content: event.target.value});
+        setMessage({ ...message, content: event.target.value });
         break;
     }
-
   };
 
   return (
@@ -38,22 +44,45 @@ function Contact (props) {
         <form className="contactUsForm">
           <p className="contactUsTitle">What Can We Help You With?</p>
           <div>
-            <input className="contactUs" placeholder="Name" type="text" id="name"  onChange= { handleChange }/>
-          </div>
-          <div> 
-            <input className="contactUs" placeholder="Email" type="text" id="email" onChange= { handleChange }/>
+            <input
+              className="contactUs"
+              placeholder="Name"
+              type="text"
+              id="name"
+              onChange={handleChange}
+            />
           </div>
           <div>
-            <textarea className="contactUs" placeholder="Your Inquery" type="text" id="content"  onChange= { handleChange }/>
+            <input
+              className="contactUs"
+              placeholder="Email"
+              type="text"
+              id="email"
+              onChange={handleChange}
+            />
           </div>
           <div>
-            <input className="contactUs" type="submit" value="Send Message" id="contactUsSubmit" onSubmit={handleSubmit} />            
+            <textarea
+              className="contactUs"
+              placeholder="Your Inquery"
+              type="text"
+              id="content"
+              onChange={handleChange}
+            />
           </div>
-      </form>
+          <div>
+            <input
+              className="contactUs"
+              type="submit"
+              value="Send Message"
+              id="contactUsSubmit"
+              onClick={handleSubmit}
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
-    
 }
 
 export default Contact;
