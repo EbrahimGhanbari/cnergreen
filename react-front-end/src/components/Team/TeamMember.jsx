26a504fd6b44bebfeb4e4d1aaf80f85bcc6a8936
ghanbari <ghanbari@ualberta.ca>
2020-10-29 00:32:49 -0600

    refractor: use passing component to add new team member

--- react-front-end/src/components/Team/TeamMember.jsx
+++ react-front-end/src/components/Team/TeamMember.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import "./TeamMember.scss";
 import { AiFillLinkedin } from "react-icons/ai";
 
-function TeamMember(props) {
+function TeamMember({ member }) {
+
   return (
     <div className="team_member">
       <div className="team_img team"></div>
-      <div className="team_name team">Ali Telma</div>
-      <div className="team_title team">CO-FOUNDER ...</div>
-      <div className="team_desc team">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut ero labore et dolore.
-      </div>
-      <a href="https://www.google.ca" target="_blank" >
+      <div className="team_name team">{member.name}</div>
+      <div className="team_title team">{member.title}</div>
+      <div className="team_desc team">{member.desc}</div>
+      <a href={member.link} target="_blank">
         <AiFillLinkedin className="team_icon" />
       </a>
     </div>
