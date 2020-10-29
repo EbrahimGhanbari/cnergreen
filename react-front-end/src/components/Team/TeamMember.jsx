d14a1957b6c14e549b2f199135f6524fdce5d328
ghanbari <ghanbari@ualberta.ca>
2020-10-29 00:14:00 -0600

    fix(release): fixed the styling

--- react-front-end/src/components/Team/TeamMember.jsx
+++ react-front-end/src/components/Team/TeamMember.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./TeamMember.scss";
+import { AiFillLinkedin } from "react-icons/ai";
 
 function TeamMember(props) {
   return (
     <div className="team_member">
-      <div id="team_img"></div>
-      <div id="team_name">Ali Telma</div>
-      <div id="team_title">CO-FOUNDER ...</div>
-      <div id="team_desc">
+      <div className="team_img team"></div>
+      <div className="team_name team">Ali Telma</div>
+      <div className="team_title team">CO-FOUNDER ...</div>
+      <div className="team_desc team">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
         tempor incididunt ut ero labore et dolore.
       </div>
-      <div id="team_icon"></div>
+      <a href="https://www.google.ca" target="_blank" >
+        <AiFillLinkedin className="team_icon" />
+      </a>
     </div>
   );
 }
