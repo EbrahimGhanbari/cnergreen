391d0a853370f8ec766a16c59a6de4fea7639f1f
ghanbari <ghanbari@ualberta.ca>
2020-12-13 11:53:32 -0700

    added pics

--- client/src/components/Main/SectionGrid.jsx
+++ client/src/components/Main/SectionGrid.jsx
@@ -1,29 +1,23 @@
-import React from 'react';
-import './SectionGrid.scss';
+import React from "react";
+import "./SectionGrid.scss";
 
-
-
-
-function SectionOne ({gridInput}) {
-
+function SectionOne({ gridInput }) {
   return (
     <div className="gridContainer">
-        <div>
-            <div className="imgGrid"></div>
-            <div className="gridItemText"> { gridInput.e1[0] }</div>
-        </div>
-        <div>
-            <div className="imgGrid"></div>
-            <div className="gridItemText">{ gridInput.e2[0] }</div>
-        </div>
-        <div>
-            <div className="imgGrid"></div>
-            <div className="gridItemText">{ gridInput.e3[0] }</div>
-        </div>
-        
+      <div>
+        <img className="imgGrid" src="rig.png" alt="Italian Trulli" />
+        <div className="gridItemText"> {gridInput.e1[0]}</div>
+      </div>
+      <div>
+        <img className="imgGrid" src="foam.png" alt="Italian Trulli" />
+        <div className="gridItemText">{gridInput.e2[0]}</div>
+      </div>
+      <div>
+        <img className="imgGrid" src="stat.png" alt="Italian Trulli" />
+        <div className="gridItemText">{gridInput.e3[0]}</div>
+      </div>
     </div>
   );
-
 }
 
 export default SectionOne;
