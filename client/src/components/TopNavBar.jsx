aa1125fe9102eaa4dd621efc323f04afde344e61
ghanbari <ghanbari@ualberta.ca>
2020-12-18 10:10:18 -0700

    fix hover on navbar

--- client/src/components/TopNavBar.jsx
+++ client/src/components/TopNavBar.jsx
@@ -1,33 +1,38 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
-
 import "./TopNavBar.scss";
 
 function Contact(props) {
   return (
-    <div className="topNav">
+    <section className="topNav">
       <div>
         <img id="logo" src="cnergreenlogo.png" alt="Italian Trulli" />
       </div>
-      <div className="topButtonGroup">
-        <Link className="topButton" to="/">
-          Home
-        </Link>
-        <Link className="topButton" to="/solution">
-          Solution
-        </Link>
-        <Link className="topButton" to="/blog">
-          Blog
-        </Link>
-        <Link className="topButton" to="/team">
-          Team
-        </Link>
-        <Link className="topButton" to="/contact">
-          Contact
-        </Link>
-      </div>
-    </div>
+      <nav className="stroke topButtonGroup">
+        <ul>
+          <li>
+            <a className="topButton" href="/">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="topButton" href="/solution">
+              Solution
+            </a>
+          </li>
+          <li>
+            <a className="topButton" href="/team">
+              Team
+            </a>
+          </li>
+          <li>
+            <a className="topButton" href="/contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </section>
   );
 }
 
