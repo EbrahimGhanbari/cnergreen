6a766925ea0f2210e2c5ca8388437e28cba8cb01
ghanbari <ghanbari@ualberta.ca>
2021-01-20 17:09:51 -0700

    added new route

--- client/src/index.js
+++ client/src/index.js
@@ -5,23 +5,22 @@ import {
     Route
 } from 'react-router-dom';
 // import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
 
 // styles
-import './index.scss';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.scss";
+
 
 // components
-// import App from './App';
-import Main from './components/Main/Main';
-import Contact from './components/Contact/Contact';
-import Team from './components/Team/Team';
-import CnerFoam from './components/CnerFoam/CnerFoam';
-import WhyCnergreen from './components/WhyCnergreen/WhyCnergreen';
-// import RequestInfo from './components/Contact/RequestInfo';
-// import Questionnaire from './components/Contact/Questionnaire';
-// import EnhanceOilRecovery from './components/Solution/EnhanceOilRecovery/EnhanceOilRecovery';
-// import FractureDrivenInteraction from './components/Solution/FractureDrivenInteraction/FractureDrivenInteraction';
+
+import Main from "./components/Main/Main";
+import Contact from "./components/Contact/Contact";
+import Team from "./components/Team/Team";
+import CnerFoam from "./components/CnerFoam/CnerFoam";
+import WhyCnergreen from "./components/WhyCnergreen/WhyCnergreen";
+import Temporary from "./components/Temporary/Temporary";
+// import EnhanceOilRecovery from "./components/Solution/EnhanceOilRecovery/EnhanceOilRecovery";
+// import FractureDrivenInteraction from "./components/Solution/FractureDrivenInteraction/FractureDrivenInteraction";
 
 const routing = (
 
@@ -32,9 +31,9 @@ const routing = (
             <Route path="/contact" component={Contact} />
             <Route exact path="/cnerfoam" component={CnerFoam} />
             <Route exact path="/why_cnergreen" component={WhyCnergreen} />
-            {/*    <Route exact path="/requestInfo" component={RequestInfo} />
-                <Route exact path="/questionnaire" component={Questionnaire} />
-                <Route exact path="/enhance_oil_recovery" component={EnhanceOilRecovery} />
+            <Route exact path="/underconstruction" component={Temporary} />
+
+            {/*    <Route exact path="/enhance_oil_recovery" component={EnhanceOilRecovery} />
                 <Route exact path="/fracture_driven_interaction" component={FractureDrivenInteraction} />*/}
 
         </div>
